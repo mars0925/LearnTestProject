07a05996ba26a189fcbade5b8c43277da3575a16
Mars <mars09250148@gmail.com>
2024-03-26 16:47:30 +0800

    Initial Commit

---
+++ LearnTest/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  LearnTest
+//
+//  Created by 張宮豪 on 2024/3/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
